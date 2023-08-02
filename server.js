e55ca283fd5dd1d54a610fca8f914b01a728619b
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-08-02 11:57:31 +0100

    update server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bp = require('body-parser');
+const Book = require('./models/book');
 
 const app = express();
 app.use(cors());
@@ -29,7 +30,18 @@ app.get('/', (req, res) => {
 app.get('/books', async (req, res) => {
   const allBooks = await Book.find(req.query);
   res.status(200).json(allBooks);
-})
+});
+
+app.post('/books', async (req, res) => {
+  const newBook = new Book(req.body);
+
+  try {
+    const saveBook = await newBook.save();
+    res.status(201).json(saveBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
